5bb6520b6a35d7361ff98c9523dd7e246006765c
D3Portillo <d3portillo@gmail.com>
2024-12-05 10:26:43 +0100

    chores: adding playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,19 +1,27 @@
-import { createPublicClient, http, parseAbi } from "viem"
+import { createPublicClient, http, parseAbi, stringToHex } from "viem"
 import { arbitrumSepolia } from "viem/chains"
+import "dotenv/config"
 
-const ABI = parseAbi(["function hola() public pure returns (string memory)"])
-const ADDRESS = "0x24573fb24ababce6eaab470e24b7568ce9f1a3f3"
+const ABI = parseAbi([
+  "function hola_mundo() view returns (string)",
+  "function calldata_len() view returns (uint256)",
+  "function ping_pong(bytes32) view returns (string)",
+])
 
 const client = createPublicClient({
   chain: arbitrumSepolia,
   transport: http(),
 })
 
+// https://sepolia.arbiscan.io/address/0x6a5a8573fe27c42ce960dcb7a19cf957f0e8f837
+const CONTRACT_ADDRESS = "0x6a5a8573fe27c42ce960dcb7a19cf957f0e8f837"
+
 async function main() {
   const result = await client.readContract({
     abi: ABI,
-    address: ADDRESS,
-    functionName: "hola",
+    address: CONTRACT_ADDRESS,
+    functionName: "ping_pong",
+    args: [stringToHex("pong", { size: 32 })],
   })
 
   console.debug(`Contract: ${result}`)
